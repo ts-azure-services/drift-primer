43358e0b7648cf248a6de893e05a8b458214e763
Santosh Thomas <thomassantosh@gmail.com>
2022-01-03 13:50:10 -0800

    Changes as of 'Mon Jan  3 13:50:09 PST 2022'

--- data-manipulation/data_analysis.py
+++ data-manipulation/data_analysis.py
@@ -3,53 +3,53 @@ import numpy as np
 
 
 def load_data(source='WA_Fn-UseC_-Telco-Customer-Churn.csv'):
+    """Load original data, and key lists"""
     df = pd.read_csv(source)
+    df['TotalCharges'] = df['TotalCharges'].str.replace(r' ','0').astype(float)
+    df['Churn'] = df['Churn'].apply(lambda x: 0 if x == "No" else 1)
+    df['SeniorCitizen'] = df['SeniorCitizen'].apply(lambda x: "No" if x == 0 else "Yes")
+    df.info()
     total_cols = df.columns
+    non_attribute_cols = ['customerID', 'MonthlyCharges', 'TotalCharges', 'Churn', 'tenure']
+    attribute_cols = list( set(total_cols) - set(non_attribute_cols) )
+    return df, total_cols, attribute_cols
 
 
-    #df.info()
+def attribute_col_ratio(df=None, col_list=None):
+    """Show percent of customers represented by non-numeric attributes"""
+    for i in col_list:
+        temp_df = df['customerID'].groupby(df[i]).count().to_frame()
+        temp_df['percent'] = temp_df['customerID'] / temp_df['customerID'].sum()
+        print(f'Customer count by {i}:\n{temp_df}\n')
 
-    return df, total_cols
+
+def numeric_col_spreads(df=None, non_numeric_cols=None):
+    #non_numeric_cols.append('Churn')
+    #temp_df = df.groupby(non_numeric_cols).size()
+    #temp_df = df['Churn'].groupby(non_numeric_cols).sum()
+    temp_df = df.groupby(non_numeric_cols).describe()#sum()
+    temp_df.to_csv('sample.csv')
+    #print(temp_df)
 
 
-def attribute_ratio_to_full_count(df=None):
-    df = df.groupb
+def main():
+    # Load and format data
+    df, total_cols, attribute_cols = load_data()
 
+    # Get customer count by major attribute
+    attribute_col_ratio(df=df, col_list=attribute_cols)
 
-#relevant_cols = ['gender', 'SeniorCitizen', 'Partner', 'Dependents',
-#       'tenure', 'PhoneService', 'MultipleLines', 'InternetService',
-#       'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
-#       'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
-#       'PaymentMethod']
+    ## Get the numeric spread for numeric columns
+    #numeric_col_spreads(df=df, non_numeric_cols=attribute_cols)
+
+if __name__ == "__main__":
+    main()
 
 #for i,v in enumerate(relevant_cols):
 #    df_temp = df.groupby(['Churn', v]).size().reset_index(name='Count')
 #    print( df_temp )
 
-df_temp = df.groupby(['gender', 'SeniorCitizen', 'Contract']).\
-        agg({'MonthlyCharges': ['min','mean', 'median', 'max']})
+#df_temp = df.groupby(['gender', 'SeniorCitizen', 'Contract']).\
+#        agg({'MonthlyCharges': ['min','mean', 'median', 'max']})
 
 #df_temp = df.groupby(['gender', 'SeniorCitizen']).describe('MonthlyCharges')
-print( df_temp )
-
-# 0   customerID        7043 non-null   object 
-# 1   gender            7043 non-null   object 
-# 2   SeniorCitizen     7043 non-null   int64  
-# 3   Partner           7043 non-null   object 
-# 4   Dependents        7043 non-null   object 
-# 5   tenure            7043 non-null   int64  
-# 6   PhoneService      7043 non-null   object 
-# 7   MultipleLines     7043 non-null   object 
-# 8   InternetService   7043 non-null   object 
-# 9   OnlineSecurity    7043 non-null   object 
-# 10  OnlineBackup      7043 non-null   object 
-# 11  DeviceProtection  7043 non-null   object 
-# 12  TechSupport       7043 non-null   object 
-# 13  StreamingTV       7043 non-null   object 
-# 14  StreamingMovies   7043 non-null   object 
-# 15  Contract          7043 non-null   object 
-# 16  PaperlessBilling  7043 non-null   object 
-# 17  PaymentMethod     7043 non-null   object 
-# 18  MonthlyCharges    7043 non-null   float64
-# 19  TotalCharges      7043 non-null   object 
-# 20  Churn             7043 non-null   object
