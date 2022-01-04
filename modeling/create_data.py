f80d56aadf38c8c6086362191e84c7c44b402f73
Santosh Thomas <thomassantosh@gmail.com>
2022-01-03 18:43:44 -0800

    Changes as of 'Mon Jan  3 18:43:43 PST 2022'

--- modeling/create_data.py
+++ modeling/create_data.py
@@ -1,12 +1,13 @@
 import random, uuid
 from collections import defaultdict
 import pandas as pd
+import numpy as np
 
 # Load choice list for 16 attributes
 # Tenure, customer_id, monthly charges, total charges and churn ignored
 choice_list = {
         "gender":['Female', 'Male'],
-        "SeniorCitizen":[0,1],
+        "SeniorCitizen":['No','Yes'],
         "Partner":['No', 'Yes'],
         "Dependents":['No', 'Yes'],
         "PhoneService":['No', 'Yes'],
@@ -34,7 +35,6 @@ def load_original_data(source='./../datasets/WA_Fn-UseC_-Telco-Customer-Churn.cs
     df['Period'] = 'M0'
     #df.info()
     df.to_pickle('./../datasets/M0.pkl')
-    return df
 
 
 def generate_monthly_pull(
@@ -51,9 +51,10 @@ def generate_monthly_pull(
 
     # Take the prior base, and filter out churned customers
     prior_customers = pd.read_pickle(prior_source)
+    prior_customers = prior_customers.loc[ prior_customers['Churn'] == 0 ]
 
     # Identify the balance of customers that will churn, capped at x%
-
+    prior_customers['Churn'] = np.random.choice([0,1], size=len(prior_customers), replace=True, p=(0.74,0.26))
 
     # Increment their tenure by 1
     prior_customers['tenure'] = prior_customers['tenure'] + 1
@@ -66,6 +67,9 @@ def generate_monthly_pull(
     prior_customers['Period'] = current_period
 
     # Add the prior base to the new customer base
+    combined_df = pd.concat([prior_customers, new_customers])
+
+    return combined_df
 
 
 
@@ -80,10 +84,8 @@ def generate_new_customers(
     def random_attribute_generator(keys):
         """Generate records based upon prior attributes"""
         for i in keys:
-            if i == 'customer_id':
-                customer_records[i].append(str(uuid.uuid1()))
-            else:
-                customer_records[i].append( random.choice(choice_list[i]))
+            customer_records[i].append( random.choice(choice_list[i]))
+        customer_records['customer_id'].append(str(uuid.uuid1()))
         customer_records['tenure'] = 1
         customer_records['MonthlyCharges'] = 45.22
         customer_records['TotalCharges']= customer_records['MonthlyCharges']
@@ -98,23 +100,27 @@ def generate_new_customers(
     return pd.DataFrame(customer_records)
 
 
-
 def main():
     """Main operational flow"""
-    #cr = generate_random_records()
-    #print(cr)
 
     # Load the original dataset, mark as M0
-    m0 = load_original_data()
+    load_original_data()
 
     # M1 operations
-    df = generate_new_customers(period='M1', min_vol=10, max_vol=20)
+    prior_period='M0'
+    current_period='M1'
+    df = generate_monthly_pull(
+            prior_period= prior_period, 
+            current_period= current_period,
+            prior_source='./../datasets/' + str(prior_period) +'.pkl',
+            min_vol=1000,
+            max_vol=2000
+            )
     print(df)
 
 
 if __name__ == "__main__":
     main()
-
 
 
 
