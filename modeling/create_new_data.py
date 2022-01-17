0616eba7d6db6605e1aa0ce5bf362728232b64e4
Santosh Thomas <thomassantosh@gmail.com>
2022-01-16 18:15:11 -0800

    Changes as of 'Sun Jan 16 18:15:11 PST 2022'

--- modeling/create_new_data.py
+++ modeling/create_new_data.py
@@ -5,7 +5,7 @@ import time
 import pandas as pd
 import numpy as np
 
-start_time = time.time()
+
 def bin_column(df=None, new_col_name=None, base_col=None, number_bins=None):
     """Bin data for specific columns"""
     df[new_col_name] = pd.cut(x = df[base_col],bins=number_bins,include_lowest=True)
@@ -56,88 +56,122 @@ def integer_alignment(
                 i += 1
     return base_list, change_list, base_list_sum, change_list_sum
 
-def create_lookup():
+def transform_original_dataset():
 
-# Initial transformations
-df = pd.read_csv('./../datasets/input-data/WA_Fn-UseC_-Telco-Customer-Churn.csv')
-df['Churn'] = df['Churn'].apply(lambda x: 0 if x == "No" else 1)
-df['SeniorCitizen'] = df['SeniorCitizen'].apply(lambda x: "No" if x == 0 else "Yes")
+    # Initial transformations
+    df = pd.read_csv('./../datasets/input-data/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    df['Churn'] = df['Churn'].apply(lambda x: 0 if x == "No" else 1)
+    df['SeniorCitizen'] = df['SeniorCitizen'].apply(lambda x: "No" if x == 0 else "Yes")
 
-# Bin columns
-df = bin_column(df=df, new_col_name='tenure_bins', base_col='tenure', number_bins=10)
-df = bin_column(df=df, new_col_name='monthly_charges_bins', base_col='MonthlyCharges', number_bins=10)
-df = df.drop(['TotalCharges', 'MonthlyCharges', 'tenure'], axis=1)
-df['monthly_charges_bins'] = df['monthly_charges_bins'].astype(object)
-df['tenure_bins'] = df['tenure_bins'].astype(object)
+    # Bin columns
+    df = bin_column(df=df, new_col_name='tenure_bins', base_col='tenure', number_bins=10)
+    df = bin_column(df=df, new_col_name='monthly_charges_bins', base_col='MonthlyCharges', number_bins=10)
+    df = df.drop(['TotalCharges', 'MonthlyCharges', 'tenure'], axis=1)
+    df['monthly_charges_bins'] = df['monthly_charges_bins'].astype(object)
+    df['tenure_bins'] = df['tenure_bins'].astype(object)
 
-# Groupby unique combinations
-col_list = list(df.columns)
-non_attribute_cols = ['customerID', 'MonthlyCharges', 'Churn']
-attribute_cols = list( set(col_list) - set(non_attribute_cols) )
+    # Groupby unique combinations
+    col_list = list(df.columns)
+    non_attribute_cols = ['customerID', 'MonthlyCharges', 'Churn']
+    attribute_cols = list( set(col_list) - set(non_attribute_cols) )
+    return df, attribute_cols
 
-temp_df = df.groupby(by=attribute_cols).agg({
-    'customerID':'count',
-    'Churn':['sum']#,'count']
-    })
-temp_df.columns = ['original_customer_count', 'original_churn_sum']#, 'churn_count']
+def create_lookup(df=None, attribute_cols=None, volume=None):
 
-# Convert to new df
-new_df = temp_df.reset_index()
-new_df['original_churn_ratio'] = new_df['original_churn_sum'] / new_df['original_customer_count']
-new_df['original_customer_ratio'] = new_df['original_customer_count'] / new_df['original_customer_count'].sum()
-#new_df = new_df.drop(['customer_count', 'churn_sum'], axis=1)
+    # Groupby to get unique combinations
+    temp_df = df.groupby(by=attribute_cols).agg({
+        'customerID':'count',
+        'Churn':['sum']#,'count']
+        })
+    temp_df.columns = ['original_customer_count', 'original_churn_sum']#, 'churn_count']
 
-# Distribute customer totals
-new_vol = 6900
-new_df['new_customer_count_float'] = new_df['original_customer_ratio'] * new_vol
-new_df['new_customer_count_int'] = new_df.apply(lambda x: round_logic(x['new_customer_count_float']), axis=1)
-# Sort to get the most volume for reconciling
-new_df = new_df.sort_values(by='new_customer_count_int', ascending=False)
+    # Convert to new df
+    new_df = temp_df.reset_index()
+    new_df['original_churn_ratio'] = new_df['original_churn_sum'] / new_df['original_customer_count']
+    new_df['original_customer_ratio'] = new_df['original_customer_count'] / new_df['original_customer_count'].sum()
 
-# After resolving to integers and sorting, then finetune to get to the right population size
-cust_count_base_list = new_df['new_customer_count_int'].to_list()
-change_list = cust_count_base_list.copy()
+    # Distribute customer totals
+    new_vol = volume
+    new_df['new_customer_count_float'] = new_df['original_customer_ratio'] * new_vol
+    new_df['new_customer_count_int'] = new_df.apply(lambda x: round_logic(x['new_customer_count_float']), axis=1)
 
-base_list, change_list, base_list_sum, change_list_sum =\
-        integer_alignment(
-        base_list=cust_count_base_list,
-        change_list=change_list,
-        target= new_vol,
-        )
+    # Sort to get the most volume for reconciling
+    new_df = new_df.sort_values(by='new_customer_count_int', ascending=False)
 
-new_df['new_customer_optimized'] = change_list
-# Convert to int, so it can be used in range functions
-new_df['new_customer_optimized'] = new_df['new_customer_optimized'].astype(int)
-new_df['new_churn_customers'] = new_df['new_customer_optimized'] * new_df['original_churn_ratio']
-new_df['new_churn_customers'] = new_df.apply(\
-        lambda x: round_logic(x['new_churn_customers']),axis=1)\
-        .astype(int)
+    # After resolving to integers and sorting, then finetune to get to the right population size
+    cust_count_base_list = new_df['new_customer_count_int'].to_list()
+    change_list = cust_count_base_list.copy()
 
-list_of_records = new_df.to_dict('records')
+    # This iterates to ensure the volume specified is adjusted
+    base_list, change_list, base_list_sum, change_list_sum =\
+            integer_alignment(
+            base_list=cust_count_base_list,
+            change_list=change_list,
+            target= new_vol,
+            )
 
-final_df = pd.DataFrame()
-# Iterate through the dictionary to produce rows
-for dictionary in list_of_records:
-    # Initialize the temporary list and dataframe
-    temp_customer_list = []
-    temp_df = pd.DataFrame()
+    new_df['new_customer_optimized'] = change_list
 
-    # Iterate through each blueprint to produce rows
-    for i in range(dictionary['new_customer_optimized']):
-        temp_dict = dictionary.copy()
-        temp_dict.update({'customerID': str(uuid.uuid1())})
-        temp_customer_list.append(temp_dict)
+    # Convert to int, so it can be used in range functions
+    new_df['new_customer_optimized'] = new_df['new_customer_optimized'].astype(int)
+    new_df['new_churn_customers'] = new_df['new_customer_optimized'] * new_df['original_churn_ratio']
+    new_df['new_churn_customers'] = new_df.apply(\
+            lambda x: round_logic(x['new_churn_customers']),axis=1)\
+            .astype(int)
 
-    temp_df = temp_df.append(temp_customer_list)
-    temp_df['Churn'] = 0
-    churn_temp_df = temp_df.sample(n=dictionary['new_churn_customers'])
-    temp_df.loc[ churn_temp_df.index, 'Churn' ] = 1
+    list_of_records = new_df.to_dict('records')
 
-    # Append the temp_df results to the final dataframe
-    final_df = final_df.append(temp_df)
+    # Iterate through the dictionary to produce rows
+    final_df = pd.DataFrame()
+    for dictionary in list_of_records:
+        # Initialize the temporary list and dataframe
+        temp_customer_list = []
+        temp_df = pd.DataFrame()
 
-# Shuffle the final result, and produce output
-final_df = final_df.sample(frac=1)
-final_df = final_df.reset_index(drop=True)
-final_df.to_csv('final_df.csv', encoding='utf-8')
-print('Entire script took %s seconds' % (time.time() - start_time))
+        # Iterate through each blueprint to produce rows
+        for i in range(dictionary['new_customer_optimized']):
+            temp_dict = dictionary.copy()
+            temp_dict.update({'customerID': str(uuid.uuid1())})
+            temp_customer_list.append(temp_dict)
+
+        temp_df = temp_df.append(temp_customer_list)
+        temp_df['Churn'] = 0
+        churn_temp_df = temp_df.sample(n=dictionary['new_churn_customers'])
+        temp_df.loc[ churn_temp_df.index, 'Churn' ] = 1
+
+        # Append the temp_df results to the final dataframe
+        final_df = final_df.append(temp_df)
+
+    # Shuffle the final result, and produce output
+    final_df = final_df.sample(frac=1)
+    final_df = final_df.reset_index(drop=True)
+    final_df = final_df.drop([
+        'original_churn_ratio',
+        'original_customer_ratio',
+        'original_customer_count',
+        'original_churn_sum',
+        'new_churn_customers',
+        'new_customer_count_float',
+        'new_customer_count_int',
+        'new_customer_optimized'
+        ], axis=1)
+    final_df.to_csv('final_df.csv', encoding='utf-8', index=False)
+
+
+def main():
+    start_time = time.time()
+
+    # Load, and transform original dataset
+    df, attribute_cols = transform_original_dataset()
+
+    # Use original dataset to create a blueprint for simulating data
+    create_lookup(
+            df = df,
+            attribute_cols= attribute_cols,
+            volume = 7200
+            )
+
+    print('Entire script took %s seconds' % (time.time() - start_time))
+
+if __name__ == "__main__":
+    main()
